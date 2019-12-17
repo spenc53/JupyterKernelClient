cd477cf12ffc22cf05f2632f557c0f60a59f7417
Spencer Seeger <spencerseeger@Spencers-MacBook-Pro-2.local>
2019-12-16 22:08:38 -0600

    updates

--- src/index.ts
+++ src/index.ts
@@ -15,31 +15,58 @@ export class JupyterKernelClient {
 
     decoder: TextDecoder = new TextDecoder()
 
-    constructor() {
+    constructor(config: KernelConfig) {
+        const base_url = `${config.transport}://${config.ip}`;
+
         this.shell = new Dealer()
-        this.shell.connect("tcp://127.0.0.1:53794");
+        this.shell.connect(`${base_url}:${config.shell_port}`);
 
         this.ioPub = new Subscriber();
-        this.ioPub.connect("tcp://127.0.0.1:53795");
+        this.ioPub.connect(`${base_url}:${config.iopub_port}`);
         this.ioPub.subscribe('');
 
         this.stdin = new Dealer();
+        this.stdin.connect(`${base_url}:${config.stdin_port}`);
+
         this.control = new Dealer();
+        this.control.connect(`${base_url}:${config.control_port}`);
+
         this.heartbeat = new Request();
+        this.heartbeat.connect(`${base_url}:${config.hb_port}`);
     }
 
-    recv_message(messages: Buffer[]) {
-        for (const message of messages) {
-            console.log(this.decoder.decode(message))
+    recv_message(messages: Buffer[]): any {
+        let thing: JupyterMessage = {};
+        let decodedMessages = messages.map((message) => this.decoder.decode(message))
+
+        thing.ids = [];
+        let indexOfDelim = decodedMessages.indexOf("<IDS|MSG>");
+        for (let i = 0; i < indexOfDelim; i++) {
+            thing.ids.push(decodedMessages[i]);
         }
+        thing.delimiter = "<IDS|MSG>";
+        thing.hmac = decodedMessages[indexOfDelim + 1];
+        thing.header = JSON.parse(decodedMessages[indexOfDelim + 2]);
+        thing.parent_header = JSON.parse(decodedMessages[indexOfDelim + 3]);
+        thing.metadata = JSON.parse(decodedMessages[indexOfDelim + 4]);
+        thing.content = JSON.parse(decodedMessages[indexOfDelim + 5]);
+        if (indexOfDelim + 6 < messages.length) {
+            thing.buffer = messages.reverse()[0]; // what if it doesn't exist?
+        }
+        // console.log(thing)
+        console.log(JSON.stringify(thing.content, null, 2))
+        return thing.content;
     }
 
-    async ioPubLoop() {
+    async subscribeToIOLoop(messageReciever: MessageReciever) {
         while (true) {
             const messages = await this.ioPub.receive();
-            this.recv_message(messages);
+            const data = this.recv_message(messages);
+            messageReciever(data);
         }
     }
+
+
 
     async getKernelInfo() {
         const data = {
@@ -69,7 +96,19 @@ export class JupyterKernelClient {
     }
 }
 
-interface KernelConfig {
+
+export interface JupyterMessage {
+    ids?: any[];
+    delimiter?: string;
+    hmac?: string;
+    header?: {};
+    parent_header?: {};
+    metadata?: {};
+    content?: {};
+    buffer?: any // not sure what this is
+}
+
+export interface KernelConfig {
     shell_port: string;
     iopub_port: string;
     stdin_port: string;
@@ -81,3 +120,36 @@ interface KernelConfig {
     signature_scheme: string;
     kernel_name: string;
 }
+
+interface MessageReciever {
+    (data: any): void;
+};
+
+
+// const config: KernelConfig = {
+//     shell_port: "53794",
+//     iopub_port: "53795",
+//     stdin_port: "53796",
+//     control_port: "53797",
+//     hb_port: "53798",
+//     key: "",
+//     ip: "127.0.0.1",
+//     transport: "tcp",
+//     signature_scheme: "",
+//     kernel_name: ""
+// }
+
+// const j = new JupyterKernelClient(config);
+// j.getKernelInfo();
+// j.startIOPubLoop()
+
+
+
+
+
+
+
+
+
+// security stuff
+// https://stackoverflow.com/questions/7480158/how-do-i-use-node-js-crypto-to-create-a-hmac-sha1-hash
